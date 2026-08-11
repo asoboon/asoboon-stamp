fe1588fa590b4d36ddf23b19bb1c3c1ebf14af95
asoboon <asoboon425@gmail.com>
2026-08-11 17:48:59 +0900

    Add files via upload

--- boonjump/sw.js
+++ boonjump/sw.js
@@ -1,4 +1,4 @@
-const BUILD = "2026-08-11-highway-valkyrie-princess-assets-v5";
+const BUILD = "2026-08-11-balance-super-reachability-v7";
 const STATIC_CACHE = `boonjump-static-${BUILD}`;
 const IMAGE_CACHE = `boonjump-images-${BUILD}`;
 const PRECACHE = [
