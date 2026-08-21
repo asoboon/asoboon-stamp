60548848bc69b1eb065ff96db475ac786bd1e436
asoboon <asoboon425@gmail.com>
2026-08-21 18:17:10 +0900

    Add files via upload

--- calltime-config.js
+++ calltime-config.js
@@ -1,8 +1,9 @@
 window.ASOBOON_CALLTIME_CONFIG = Object.freeze({
-  version: "5.1.3",
+  version: "5.2.0",
   // Apps Scriptのウェブアプリをデプロイ後、/exec URLに置換
   webAppUrl: "https://script.google.com/macros/s/AKfycbxkI54t2MdxrF-y7JQ-FhwUL4xsHvxWyxfZrQmn5Uu4JzKXhgm0QT8W7sVXpXzkpOKwWA/exec",
   parkingUrl: "./home.html?mode=before&view=parking",
+  callWindowMinutes: 30,
   monitorPollMs: 2000,
   safetyFullPollMs: 15000,
   degradedPollMs: 5000,
