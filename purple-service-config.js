443c3375c01dd55c902f38421813408e97d7ed92
asoboon <asoboon425@gmail.com>
2026-09-06 20:43:09 +0900

    purple: align client config with backend v1.2

--- purple-service-config.js
+++ purple-service-config.js
@@ -3,12 +3,12 @@
  * NEVER put Channel Secret, AirWAIT API key, access tokens, or notification tokens here.
  */
 window.ASOBOON_PURPLE_SERVICE_CONFIG=Object.freeze({
-  version:'1.1.0',
+  version:'1.2.1',
   liffId:'2011467470-Gk5C3lWf',
   liffUrl:'https://miniapp.line.me/2011467470-Gk5C3lWf',
   backendUrl:'',
-  backendVersionPrefix:'1.1.',
-  requestTimeoutMs:9000,
-  statusPollMs:[300,600,1000,1600,2400],
+  backendVersionPrefix:'1.2.',
+  requestTimeoutMs:10000,
+  statusPollMs:[300,600,1000,1600,2400,3500],
   resultKey:'asoboon_purple_service_message_v1'
 });
