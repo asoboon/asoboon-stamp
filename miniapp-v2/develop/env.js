e2e5d4f370742188932606068d9ada1367717bf2
asoboon <asoboon425@gmail.com>
2026-09-11 11:40:35 +0900

    test: publish isolated Developing MINI App env

---
+++ miniapp-v2/develop/env.js
@@ -0,0 +1,28 @@
+/* ASOBooN LINE MINI App v2 / Developing environment
+ * Public identifiers only. Never put secrets in this file.
+ */
+(()=>{'use strict';
+window.ASOBOON_V2_ENV=Object.freeze({
+  environment:'develop',
+  environmentLabel:'DEVELOPING',
+  channelId:'2009884611',
+  liffId:'2009884611-bDgDzGrN',
+  liffUrl:'https://miniapp.line.me/2009884611-bDgDzGrN',
+  endpoint:'https://asoboon.github.io/asoboon-stamp/miniapp-v2/develop/',
+  backendUrl:'',
+  backendEnvironment:'official-develop',
+  featureFlags:Object.freeze({
+    reception:false,
+    callstatus:false,
+    serviceMessage:false,
+    timeguide:true,
+    firstGuide:true,
+    entryGuide:true,
+    rules:true,
+    stamp:false,
+    omikuji:false,
+    game:false,
+    parking:true
+  })
+});
+})();
