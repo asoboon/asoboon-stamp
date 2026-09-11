76cea4da9700700b3b35b05dc93e38f58717c67f
asoboon <asoboon425@gmail.com>
2026-09-11 11:48:40 +0900

    chore: bump new HOME build version

--- miniapp-v2/shared/config-common.js
+++ miniapp-v2/shared/config-common.js
@@ -17,7 +17,7 @@ const ROUTES=Object.freeze({
 });
 const CONFIG=Object.freeze({
   appName:'ASOBooN',
-  version:'2.0.0-devhome',
+  version:'2.0.1-newhome',
   timeZone:'Asia/Tokyo',
   routes:ROUTES,
   prices:Object.freeze({adult:600,child:900,infant:0}),
