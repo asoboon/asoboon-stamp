8f0ab1aa44bc774bff3655ca12182e1caab854d9
asoboon <asoboon425@gmail.com>
2026-09-11 11:37:20 +0900

    feat: add Published MINI App environment

---
+++ miniapp-v2/published/env.js
@@ -0,0 +1,18 @@
+/* ASOBooN LINE MINI App v2 / Published environment
+ * Public identifiers only. Never put secrets in this file.
+ */
+(()=>{'use strict';
+window.ASOBOON_V2_ENV=Object.freeze({
+  environment:'published',
+  environmentLabel:'PUBLISHED',
+  channelId:'2009884613',
+  liffId:'2009884613-ELc6kolf',
+  liffUrl:'https://miniapp.line.me/2009884613-ELc6kolf',
+  endpoint:'https://asoboon.github.io/asoboon-stamp/miniapp-v2/published/',
+  backendUrl:'',
+  backendEnvironment:'official-published',
+  featureFlags:Object.freeze({
+    reception:false,callstatus:false,serviceMessage:false,timeguide:true,firstGuide:true,entryGuide:true,rules:true,stamp:false,omikuji:false,game:false,parking:true
+  })
+});
+})();
