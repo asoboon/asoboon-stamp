678507ebb49f3fa767482624ca9a84cadc7348e1
asoboon <asoboon425@gmail.com>
2026-09-11 11:37:11 +0900

    feat: add Review MINI App environment

---
+++ miniapp-v2/review/env.js
@@ -0,0 +1,18 @@
+/* ASOBooN LINE MINI App v2 / Review environment
+ * Public identifiers only. Never put secrets in this file.
+ */
+(()=>{'use strict';
+window.ASOBOON_V2_ENV=Object.freeze({
+  environment:'review',
+  environmentLabel:'REVIEW',
+  channelId:'2009884612',
+  liffId:'2009884612-hhM4k4GP',
+  liffUrl:'https://miniapp.line.me/2009884612-hhM4k4GP',
+  endpoint:'https://asoboon.github.io/asoboon-stamp/miniapp-v2/review/',
+  backendUrl:'',
+  backendEnvironment:'official-review',
+  featureFlags:Object.freeze({
+    reception:false,callstatus:false,serviceMessage:false,timeguide:true,firstGuide:true,entryGuide:true,rules:true,stamp:false,omikuji:false,game:false,parking:true
+  })
+});
+})();
